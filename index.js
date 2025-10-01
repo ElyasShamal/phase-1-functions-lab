451a851e5158be65e5688ba6d19f3c3d5bdd9d36
ElyasShamal <Elyas.shamal2@gmail.com>
2025-10-01 01:03:30 -0400

    function lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distance){
+    const hq = 42;
+    return Math.abs(distance - hq);
+}
+
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(start, distination){
+    return Math.abs(start - distination) * 264;
+}
+
+function calculatesFarePrice(start, distination){
+    const distance = distanceTravelledInFeet(start, distination);
+    if (distance <= 400){
+        return 0;
+    }else if(distance > 400 && distance <= 2000){
+        return (distance - 400) * 0.02;
+    }else if (distance > 2000 && distance <= 2500){
+        return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+}
